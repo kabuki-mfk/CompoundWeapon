097c2ef2b49a8dd04e59914335f60f5087d81c11
kabuki-mfk <kbl_mfk@outlook.com>
2021-02-22 10:24:59 +0800

    add DataObject impl IDataObject

---
+++ src/main/java/com/github/kabuki/compoundweapon/common/data/DataObject.java
@@ -0,0 +1,37 @@
+package com.github.kabuki.compoundweapon.common.data;
+
+import com.github.kabuki.compoundweapon.api.data.IDataObject;
+
+public class DataObject implements IDataObject {
+
+    private Object value;
+
+    public DataObject(Object objIn)
+    {
+        checkType(objIn);
+        if(objIn instanceof DataObject)
+        this.value = objIn;
+    }
+
+    private final void checkType(Object object)
+    {
+        if(object instanceof DataObject)
+            throw new IllegalArgumentException("object class cannot be DataObject");
+    }
+
+    @Override
+    public Class<?> getDataType() {
+        return value == null ? DataObject.class : value.getClass();
+    }
+
+    @Override
+    public void set(Object value) {
+        checkType(value);
+        this.value = value;
+    }
+
+    @Override
+    public <T> T get(Class<T> var_class) {
+        return getDataType() == DataObject.class ? null : (T) value;
+    }
+}
