d0ba417271da6e50941a1335d995bd6e061e1c10
kabuki-mfk <67349826+kabuki-mfk@users.noreply.github.com>
2021-03-18 09:27:36 +0800

    feat: add ISideTaskService
    ISideTaskService is provide services for DelayedTask

---
+++ src/main/java/com/github/kabuki/compoundweapon/api/skill/service/ISideTaskService.java
@@ -0,0 +1,14 @@
+package com.github.kabuki.compoundweapon.api.skill.service;
+
+import net.minecraft.entity.Entity;
+import net.minecraftforge.fml.relauncher.Side;
+
+public interface ISideTaskService {
+    Side getSide();
+
+    boolean scheduledRelease(int id, long unix, Runnable callback_fun);
+
+    boolean interruptTask(Entity interruptTarget);
+
+    DelayedTask getTask(int id);
+}
