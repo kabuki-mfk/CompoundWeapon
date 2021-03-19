f0e842b0ecc14c8026c7e6282534d5474962a81c
kabuki-mfk <67349826+kabuki-mfk@users.noreply.github.com>
2021-03-19 14:45:03 +0800

    feat: impl IWeaponMaterial
    add WeaponMaterial implements IWeaponMaterial, Cloneable

---
+++ src/main/java/com/github/kabuki/compoundweapon/weapon/material/WeaponMaterial.java
@@ -0,0 +1,50 @@
+package com.github.kabuki.compoundweapon.weapon.material;
+
+import com.github.kabuki.compoundweapon.CompoundWeapon;
+import com.github.kabuki.compoundweapon.api.weapon.IWeaponMaterial;
+import com.github.kabuki.compoundweapon.api.weapon.data.IWeaponAttributes;
+import com.github.kabuki.compoundweapon.weapon.attribute.Attribute;
+import com.github.kabuki.compoundweapon.weapon.attribute.WeaponAttributes;
+
+public class WeaponMaterial implements IWeaponMaterial, Cloneable {
+
+    private IWeaponAttributes attributes = new WeaponAttributes();
+    private String name;
+
+    public WeaponMaterial(float damage, float speed, int durability) {
+        attributes.setAttribute("damage", new Attribute(damage));
+        attributes.setAttribute("speed", new Attribute(speed));
+        attributes.setAttribute("durability", new Attribute(durability));
+    }
+
+    public WeaponMaterial(String name, float damage, float speed, int durability) {
+        this(damage, speed, durability);
+        setName(name);
+    }
+
+    public WeaponMaterial(String name, IWeaponAttributes attributes) {
+        setName(name);
+        this.attributes = attributes;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public IWeaponAttributes getAttributeInstance() {
+        return attributes;
+    }
+
+    @Override
+    public Object clone() {
+        Object clone = null;
+        try {
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            CompoundWeapon.LOGGER.error("it cannot be hannpen");
+        }
+        return clone;
+    }
+}
