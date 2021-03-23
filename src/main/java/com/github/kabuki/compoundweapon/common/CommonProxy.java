36b71110e4db26028571c45ed2c6c78326108184
kabuki-mfk <67349826+kabuki-mfk@users.noreply.github.com>
2021-03-23 10:22:06 +0800

    feat: add capability registration

--- src/main/java/com/github/kabuki/compoundweapon/common/CommonProxy.java
+++ src/main/java/com/github/kabuki/compoundweapon/common/CommonProxy.java
@@ -1,5 +1,12 @@
 package com.github.kabuki.compoundweapon.common;
 
+import com.github.kabuki.compoundweapon.api.skill.ISkillProvider;
+import com.github.kabuki.compoundweapon.common.capability.CapabilitySkillCDTracker;
+import com.github.kabuki.compoundweapon.common.capability.CapabilitySkillProvider;
+import com.github.kabuki.compoundweapon.weapon.skill.SkillProvider;
+import com.github.kabuki.compoundweapon.weapon.skill.service.ISkillCDTracker;
+import com.github.kabuki.compoundweapon.weapon.skill.service.SkillCDTracker;
+import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -7,7 +14,8 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 public class CommonProxy {
     public void preInit(FMLPreInitializationEvent event) {
-
+        CapabilityManager.INSTANCE.register(ISkillProvider.class, new CapabilitySkillProvider.Storage(), SkillProvider::new);
+        CapabilityManager.INSTANCE.register(ISkillCDTracker.class, new CapabilitySkillCDTracker.Storage(), SkillCDTracker::new);
     }
 
     public void init(FMLInitializationEvent event) {
