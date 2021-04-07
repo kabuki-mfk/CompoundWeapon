6864d663e6c0a1af58ed0ba1c6606ada48065d01
kabuki-mfk <67349826+kabuki-mfk@users.noreply.github.com>
2021-04-07 09:23:10 +0800

    feat: Supplement WeaponType
    add typeName
    add builder() on WeaponType, builder() return AbstractWeaponBuilder used to create a weapon of the corresponding type

--- src/main/java/com/github/kabuki/compoundweapon/api/weapon/WeaponType.java
+++ src/main/java/com/github/kabuki/compoundweapon/api/weapon/WeaponType.java
@@ -1,5 +1,122 @@
 package com.github.kabuki.compoundweapon.api.weapon;
 
+import com.github.kabuki.compoundweapon.CompoundWeapon;
+import com.github.kabuki.compoundweapon.api.skill.ISkill;
+import com.github.kabuki.compoundweapon.weapon.combat.melee.Sword;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.Objects;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public enum WeaponType {
-    SWORD
+    SWORD("sword", new AbstractWeaponBuilder<IWeapon>() {
+        @Override
+        public IWeapon build() {
+            Sword sword = new Sword(material);
+            if(hasOverrideFlag(DAMAGE))
+            {
+                sword.setOverrideDamage(damage);
+            }
+            if(hasOverrideFlag(DURABILITY))
+            {
+                sword.setOverrideDurability(durability);
+            }
+            if(hasOverrideFlag(SPEED))
+            {
+                sword.setOverrideSpeed(speed);
+            }
+            return sword;
+        }
+    });
+
+    private final String typeName;
+    private final AbstractWeaponBuilder<? extends IWeapon> factory;
+
+    WeaponType(String name, AbstractWeaponBuilder<? extends IWeapon> weaponFactory) {
+        this.typeName = name;
+        this.factory = weaponFactory;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public AbstractWeaponBuilder<? extends IWeapon> builder() {
+        return (AbstractWeaponBuilder<? extends IWeapon>) factory.clone();
+    }
+
+    public abstract static class AbstractWeaponBuilder<T extends IWeapon> implements Cloneable
+    {
+        protected IWeaponMaterial material;
+        protected final List<ISkill> skills = Lists.newArrayList();
+        protected String name;
+
+        protected float speed;
+        protected float damage;
+        protected int durability;
+
+        protected static final byte DAMAGE = 0x01;
+        protected static final byte DURABILITY = 0x02;
+        protected static final byte SPEED = 0x04;
+        private byte override_flag = 0;
+        
+        protected void setOverrideFlag(byte flag) {
+            override_flag |= flag;
+        }
+
+        protected boolean hasOverrideFlag(byte flag) {
+            return (override_flag & flag) != 0;
+        }
+
+        public AbstractWeaponBuilder<T> material(IWeaponMaterial material) {
+            this.material = checkNotNull(material, "material");
+            return this;
+        }
+
+        public AbstractWeaponBuilder<T> speed(float speed) {
+            this.speed = speed;
+            setOverrideFlag(SPEED);
+            return this;
+        }
+
+        public AbstractWeaponBuilder<T> damage(float damage) {
+            this.damage = damage;
+            setOverrideFlag(DAMAGE);
+            return this;
+        }
+
+        public AbstractWeaponBuilder<T> durability(int durability) {
+            this.durability = durability;
+            setOverrideFlag(DURABILITY);
+            return this;
+        }
+
+        public AbstractWeaponBuilder<T> skill(ISkill skill) {
+            Objects.requireNonNull(skill, "skill");
+            if(skills.contains(skill))
+                throw new IllegalArgumentException("skill already exists");
+            skills.add(skill);
+            return this;
+        }
+
+        public AbstractWeaponBuilder<T> name(String name) {
+            this.name = checkNotNull(name, "name");
+            return this;
+        }
+
+        public abstract T build();
+
+        @Override
+        protected Object clone() {
+            Object clone = null;
+            try {
+                clone = super.clone();
+            } catch (CloneNotSupportedException e) {
+                CompoundWeapon.LOGGER.error("it cannot happen", e);
+            }
+            return clone;
+        }
+    }
 }
