8cd5b38480e934b9d337405b4f4a549abfa811c0
kabuki-mfk <67349826+kabuki-mfk@users.noreply.github.com>
2021-04-29 23:23:47 +0800

    fix: Removed redundant preload files method

--- src/main/java/com/github/kabuki/compoundweapon/client/model/ModelPack.java
+++ src/main/java/com/github/kabuki/compoundweapon/client/model/ModelPack.java
@@ -1,15 +1,13 @@
 package com.github.kabuki.compoundweapon.client.model;
 
 import com.google.common.base.CharMatcher;
-import com.google.common.collect.Lists;
+
 import net.minecraft.client.resources.ResourcePackFileNotFoundException;
 import net.minecraft.util.Util;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.io.*;
-import java.util.List;
-import java.util.function.Predicate;
 
 @SideOnly(Side.CLIENT)
 public class ModelPack {
@@ -18,26 +16,9 @@ public class ModelPack {
     private static final CharMatcher BACKSLASH_MATCHER = CharMatcher.is('\\');
 
     protected final File dir;
-    private List<File> list = Lists.newArrayList();
-    private Predicate<File> filter;
 
     public ModelPack(File modelDir) {
-        this(modelDir, (f) -> true);
-    }
-
-    public ModelPack(File modelDir, Predicate<File> filter) {
-        this.filter = filter;
         this.dir = modelDir;
-        loadFiles(modelDir);
-    }
-
-    private void loadFiles(File dir) {
-        for (File f : dir.listFiles()) {
-            if (f.isDirectory())
-                loadFiles(f);
-            else if (filter.test(f))
-                list.add(f);
-        }
     }
 
     private boolean validatePath(File file, String path) throws IOException {
