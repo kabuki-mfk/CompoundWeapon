84f5439eed977e3b204b1cb1568b8502608b2793
kabuki-mfk <67349826+kabuki-mfk@users.noreply.github.com>
2021-05-07 16:55:35 +0800

    fix: modified root directory of the model

--- src/main/java/com/github/kabuki/compoundweapon/client/model/CustomResourceLocation.java
+++ src/main/java/com/github/kabuki/compoundweapon/client/model/CustomResourceLocation.java
@@ -2,15 +2,30 @@ package com.github.kabuki.compoundweapon.client.model;
 
 import net.minecraft.util.ResourceLocation;
 
-public class CustomResourceLocation extends ResourceLocation {
+import java.io.File;
 
+public class CustomResourceLocation extends ResourceLocation {
+    public static File modelDir;
     private final ModelType modelType;
     public CustomResourceLocation(String path, ModelType type) {
-        super("./mods/CompoundWeapon/Custom/Model", path);
+        super(modelDir.getPath(), path);
         this.modelType = type;
     }
 
     public ModelType getModelType() {
         return modelType;
+    }
+
+    public String getLocation()
+    {
+        return this.namespace + ":" + this.path;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomResourceLocation{" +
+                "location=" + getLocation() +
+                "modelType=" + modelType +
+                '}';
     }
 }
