492590e934585dd7004ff6474b6fd547177011a6
kabuki-mfk <67349826+kabuki-mfk@users.noreply.github.com>
2021-05-27 17:54:40 +0800

    fix: Modify weapon registration

--- src/main/java/com/github/kabuki/compoundweapon/common/registries/RegistryEventHandler.java
+++ src/main/java/com/github/kabuki/compoundweapon/common/registries/RegistryEventHandler.java
@@ -1,7 +1,7 @@
 package com.github.kabuki.compoundweapon.common.registries;
 
 import com.github.kabuki.compoundweapon.CompoundWeapon;
-import com.github.kabuki.compoundweapon.weapon.Weapon;
+import com.github.kabuki.compoundweapon.api.weapon.IWeapon;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
@@ -21,10 +21,12 @@ public class RegistryEventHandler {
     @SubscribeEvent
     public static void onRegisterItem(RegistryEvent.Register<Item> event)
     {
-        for(Map.Entry<String, Weapon> entry : WeaponRegistry.getInstance().getRegistry().entrySet())
+        for(Map.Entry<String, IWeapon> entry : WeaponRegistry.getInstance().getRegistry().entrySet())
         {
-            Weapon regWeapon = entry.getValue();
-            event.getRegistry().register(regWeapon.setRegistryName(CompoundWeapon.MOD_ID, entry.getKey()));
+            if(entry.getValue() instanceof Item) {
+                Item regWeapon = (Item) entry.getValue();
+                event.getRegistry().register(regWeapon.setRegistryName(CompoundWeapon.MOD_ID, entry.getKey()));
+            }
         }
     }
 }
